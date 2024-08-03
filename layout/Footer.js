48a585ea9f81f6a0413f5d228085016864e0e367
codelightII <codelight001@gmail.com>
2024-08-03 11:19:52 +0100

    fixed stuff

--- layout/Footer.js
+++ layout/Footer.js
@@ -5,7 +5,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white mt-40 p-auto flex flex-col w-[480px]">
+    <footer className="bg-black text-white mt-40 p-auto flex flex-col w-[180px]">
       <div className=" text-5xl font-bold mt-8 ml-6 mb-6">Custina</div>
 
       <div className="flex ml-10 flex-col ">
