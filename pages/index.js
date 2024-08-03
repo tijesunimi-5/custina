070fff3ee65abc099f64b04eba8b03520cff952b
codelightII <codelight001@gmail.com>
2024-08-03 18:55:20 +0100

    fixed stuff

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ export default function Home() {
           </motion.p>
         </div>
 
-        <div className="w-[600px] m-auto">
+        <div className="w-[600px] m-auto mr-3">
           <motion.h1
             initial={{
               y: 20,
